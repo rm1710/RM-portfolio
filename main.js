0cdb0976bebb3cf1d50c1b2fc69e10705807baa1
rm1710 <d290mishraritesh@gmail.com>
2024-06-30 18:23:10 +0530

    d

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var typed = new Typed(".text", {
-    strings:["Backend and Frontend Developer", "Youtuber", "Web Developer"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
+    strings:["Web Developer", "Backend and Frontend Developer", "Youtuber" ],
+    typeSpeed:30,
+    backSpeed:80,
+    backDelay:1200,
     loop:true,
 });
