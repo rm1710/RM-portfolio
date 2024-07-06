4089015416ee99175cbce90b09b350d88e24ed34
rm1710 <d290mishraritesh@gmail.com>
2024-07-06 17:42:42 +0530

    d

--- main.js
+++ main.js
@@ -5,3 +5,21 @@ var typed = new Typed(".text", {
     backDelay:1200,
     loop:true,
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu');
+    const openMenu = document.querySelector('.open-menu');
+    const closeMenu = document.querySelector('.close-menu');
+
+    openMenu.addEventListener('click', () => {
+        menu.classList.add('active');
+        openMenu.classList.add('active');
+        closeMenu.classList.add('active');
+    });
+
+    closeMenu.addEventListener('click', () => {
+        menu.classList.remove('active');
+        openMenu.classList.remove('active');
+        closeMenu.classList.remove('active');
+    });
+});
